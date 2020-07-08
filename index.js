a16fb326dc6f1805592ca8c04ae06ed731042018
jincy <jincy.shone@wipro.com>
2020-07-08 09:48:59 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send({"hello":"there"});
+})
+
+
+
+
+const port = process.env.port || 5000;
+app.listen(port);
